f71013bac0809a956b1b35fd8d15f9da313910a7
Gilberto Medeiros <gilberto.medeiros@compasso.com.br>
2025-06-17 11:19:42 -0300

    feat: starting the configuration of configuration folders and files

---
+++ src/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConnectionDB = ""
+	Port               = 0
+	SecretKey          []byte
+)
+
+func loadConfig() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 5000
+	}
+
+	StringConnectionDB = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
+
+}
